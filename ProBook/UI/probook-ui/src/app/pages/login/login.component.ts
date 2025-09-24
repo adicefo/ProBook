4575977dc9ba5c311760ae6d7316d6268728355b
Adi Ćefo <167250418+adicefo@users.noreply.github.com>
2025-09-24 23:24:26 +0200

    Finished UI for login component
    Finished UI for login component

--- ProBook/UI/probook-ui/src/app/pages/login/login.component.ts
+++ ProBook/UI/probook-ui/src/app/pages/login/login.component.ts
@@ -1,12 +1,28 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../material.module';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
 
+  submit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
